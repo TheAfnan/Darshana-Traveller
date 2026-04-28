d91f63dab19db0924eee582b63ddf1223444df5a
Mohd Afnan <afnan3081n@gmail.com>
2026-04-28 17:58:47 +0530

    Restore original App.tsx - deploy full DarShana travel platform

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.minimal';
+import './i18n';
+import App from './App';
 import { ErrorBoundary } from './components/ErrorBoundary';
 
 const rootElement = document.getElementById('root');
